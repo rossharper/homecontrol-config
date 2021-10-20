526fbc51b93e0590ef5e4b36a692a754e70c6c2d
Ross Harper <ross@rosss-mbp-16.home>
2021-10-20 18:02:48 +0100

    Add instructions for missing serial port setup

--- xrfsensorcomms/install.sh
+++ xrfsensorcomms/install.sh
@@ -11,4 +11,8 @@ sudo npm install serialport -g --unsafe-perm --build-from-source
 npm install --production
 sh installDaemon.sh
 
+# TODO serial port setup missing?
+# Error: Permission denied, cannot open /dev/ttyAMA0
+# sudo raspi-config and turn on serial port
+
 popd
